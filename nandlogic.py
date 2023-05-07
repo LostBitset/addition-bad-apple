7caa1aac95208d92a6edf237852a067c1bd57714
Landon <102832670+LostBitset@users.noreply.github.com>
2023-05-07 19:56:36 -0400

    alright

--- nandlogic.py
+++ nandlogic.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
 import typing as t
 
+def nand(xs: t.List[bool]) -> bool:
+    return not all(xs)
+
 Var = str
 
 @dataclass
@@ -11,12 +14,36 @@ class NandOp:
 @dataclass
 class NandInstance:
     circuit: "NandCircuit"
-    ibindings: t.Dict[Var, Var]
-    obindings: t.Dict[Var, Var]
+    ibindings: t.Dict[Var, Var]  # inner -> outer
+    obindings: t.Dict[Var, Var]  # inner -> outer
 
 @dataclass
 class NandCircuit:
     inputs: t.List[Var]
     outputs: t.List[Var]
     ops: t.List[NandOp | NandInstance]
+
+    def eval(self, ivals: t.Dict[Var, bool]) -> t.Dict[Var, bool]:
+        state = ivals.copy()
+        for op in self.ops:
+            if isinstance(op, NandOp):
+                state[op.target] = nand([
+                    state[var] for var in op.inputs
+                ])
+            elif isinstance(op, NandInstance):
+                outputs = op.circuit.eval({
+                    inner: state[outer]
+                    for inner, outer in op.ibindings.entries()
+                })
+                for inner, value in outputs:
+                    state[op.obindings[inner]] = value
+            else:
+                raise BadNandOpError(op)
+
+class BadNandOpError(BaseException):
+    def __init__(self, op: t.Any):
+        self.op = op
+
+    def __str__(self) -> str:
+        return "Bad nand op: {repr(op)}."
 
