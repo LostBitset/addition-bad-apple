6ebf7ab4a26715e83b1425cdc5211ecd8098f870
Landon <102832670+LostBitset@users.noreply.github.com>
2023-05-08 12:04:06 -0400

    saving

--- cla_test.py
+++ cla_test.py
@@ -1,28 +1,9 @@
 from cla import carry_lookahead_adder
+from adder_utils import make_adder_inputs
 
 def main():
     cla = carry_lookahead_adder(3)
-    inputs = {
-
-        "a 0": True,
-        "a 1": False,
-        "a 2": False,
-        "a 3": False,
-        "a 4": False,
-        "a 5": False,
-        "a 6": False,
-        "a 7": False,
-
-        "b 0": True,
-        "b 1": False,
-        "b 2": False,
-        "b 3": False,
-        "b 4": False,
-        "b 5": False,
-        "b 6": False,
-        "b 7": False,
-
-    }
+    inputs = make_adder_inputs(69, 80)
     outputs = cla.eval(inputs, True)
     print("".join([
         "1" if outputs[f"sum {i}"] else "0"
