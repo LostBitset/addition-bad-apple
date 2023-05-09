2f648c625cbf083c3099dfef7d6db9c2241f0bc0
Landon <102832670+LostBitset@users.noreply.github.com>
2023-05-08 20:13:09 -0400

    that wasnt a problem but we have a new one

--- flatten_test.py
+++ flatten_test.py
@@ -1,9 +1,16 @@
 from nandlogic_std import xor_g
+from cla import carry_lookahead_adder
 from pprint import pprint
 
 def main():
-    netlist = xor_g.flatten()
-    pprint(netlist)
+    netlist1 = xor_g.flatten()
+    print("===  BEGIN NETLIST: XOR_G  ===")
+    pprint(netlist1)
+    print("===   END NETLIST: XOR_G   ===")
+    netlist2 = carry_lookahead_adder(3).flatten()
+    print("===  BEGIN NETLIST: CARRY_LOOKAHEAD_ADDER(3)  ===")
+    pprint(netlist2)
+    print("===   END NETLIST: CARRY_LOOKAHEAD_ADDER(3)   ===")
 
 if __name__ == "__main__":
     main()
