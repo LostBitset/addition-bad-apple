7beb5569918aa57d2f6fe833a393ebd1338b2b94
Landon <102832670+LostBitset@users.noreply.github.com>
2023-05-10 11:14:23 -0400

    saving work

---
+++ serialize_netlists.py
@@ -0,0 +1,21 @@
+from nandlogic import Var, NandNetlist, NandOp
+import typing as t
+
+def serialize_netlist(netlist: NandNetlist) -> t.Generator[str, None, None]:
+    next_id = 0
+    ids = dict()
+    def generate_id():
+        curr_id = next_id
+        next_id += 1
+        return curr_id
+    def get_id(key: Var):
+        if key in ids:
+            return ids[key]
+        chosen_id = generate_id()
+        ids[key] = chosen_id
+        return chosen_id
+    merge = " ".join
+    def serialize_gate_of_netlist(op: NandOp) -> str:
+        line = f"nand {merge(get_id(i) for i in op.inputs)} ~> {get_id(op.target)}"
+    return "\n".join(serialize_gate_of_netlist(op) for op in netlist.gates)
+
