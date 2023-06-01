529bf4dbeb18fee08c85368592dcc9fe41052a6c
Landon <102832670+LostBitset@users.noreply.github.com>
2023-06-01 11:57:47 -0400

    so close :)

--- place.py
+++ place.py
@@ -1,8 +1,37 @@
+from nandlogic import NandNetlist, NandOp
+from serialize_netlists import serialize_netlist
+
+from positioner.positions import get_positions
+
 import pickle as pkl
+import sys
+
+frame = int(sys.argv[1])
 
 netlists = None
 with open("netlists.pkl", "rb") as f:
     netlists = pkl.load(f)
 
-print(netlists)
+result, gates = None, None
+for levels in range(5, 0, -1):
+    gates = netlists[levels]
+    result = get_positions(frame, len(gates), "positioner/frames")
+    if result is not None:
+        break
+
+if (result is None) or (result == []):
+    ser = "SKIPME"
+else:
+    netlist_lines = serialize_netlist(
+        NandNetlist(gates)
+    ).split("\n")
+
+    lines = []
+    for i, op in enumerate(netlist_lines):
+        (x, y) = result[i]
+        lines.append(f"{op} @ {x},{y}")
+
+    ser = "\n".join(lines)
+
+print(ser)
 
