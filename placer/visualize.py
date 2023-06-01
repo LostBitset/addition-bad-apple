3ad464a95fdc32c6c34525e266bdbf3fd4cdc6b8
Landon <102832670+LostBitset@users.noreply.github.com>
2023-06-01 08:17:34 -0400

    methinks its time

--- placer/visualize.py
+++ placer/visualize.py
@@ -4,6 +4,7 @@ import numpy as np
 import cv2
 
 import sys
+import random
 
 gates = None
 with open(sys.argv[1], "r") as f:
@@ -37,7 +38,7 @@ for i, gate in enumerate(gates):
 
 print(wires)
 
-tot, pad = int(480 / 20), 5
+tot, pad = int(480 / 20), 10
 sc = tot - pad
 
 def port_position(port, posmap):
@@ -54,6 +55,8 @@ gate_positions = []
 
 for gate in gates:
     x, y = gate.x * (sc + pad), gate.y * (sc + pad)
+    x += random.randint(-(pad // 2) + 1, (pad // 2))
+    y += random.randint(-(pad // 2) + 1, (pad // 2))
     w, h = sc, sc
     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
     gate_positions.append((x, y))
@@ -63,6 +66,9 @@ for wire in wires:
     a_pos = port_position(a, gate_positions)
     b_pos = port_position(b, gate_positions)
     cv2.line(img, a_pos, b_pos, (0, 0, 255), 1)
+
+translate = np.float32([[1, 0, (pad // 2)], [0, 1, (pad // 2)]])
+img = cv2.warpAffine(img, translate, (img.shape[1], img.shape[0]))
 
 cv2.imshow("Test", img)
 cv2.waitKey(0)
