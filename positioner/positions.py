0131a7d9392f08863c1bf5d5c349516779e79fbf
Landon <102832670+LostBitset@users.noreply.github.com>
2023-06-01 09:35:34 -0400

    things

---
+++ positioner/positions.py
@@ -0,0 +1,25 @@
+from PIL import Image
+import numpy as np
+
+import sys
+
+frame = int(sys.argv[1])
+
+pic = Image.open(f"frames/BadApple_{frame}.jpg", "r")
+img = np.asarray(pic)[:,:,0] / 255
+
+area = img.sum() / img.size
+
+width = None
+if area < 0.005:
+    print("nothing")
+    sys.exit(0)
+else:
+    width = 1
+
+counts = {
+    1: 2, # FIXME
+}[width]
+
+density = counts / area
+
